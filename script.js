137a9c4fc8972ff7262e39917b7d4b3211c17c88
kmurp62rulz <kmurp62rulz@gmail.com>
2022-01-08 22:20:36 +1000

    Bug fix

--- script.js
+++ script.js
@@ -12,25 +12,32 @@ function checkValid(){
         fuel = Math.ceil(miles * 0.48823529)
         if (miles > 680) {
             //alert("Nautical miles must be less than 680!")
-            error.innerHTML = "Nautical miles must be less than 680!";
+            error.innerHTML = "Nautical miles must be less than 680";
             numberGenerator.innerHTML = 0;
         } else {
             numberGenerator.innerHTML = fuel;
+            error.innerHTML = "";
         }
         
     } else if (extra_fuel === 'extra_true') {
         fuel = Math.ceil(miles * 0.48823529) + 3
         if (miles > 680) {
             //alert("Nautical miles must be less than 680!")
-            error.innerHTML = "Nautical miles must be less than 680!";
+            error.innerHTML = "Nautical miles must be less than 680";
             numberGenerator.innerHTML = 0;
         } else {
             numberGenerator.innerHTML = fuel;
+            error.innerHTML = "";
         }
     }
 }
 
-
+/* button.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        checkValid()
+    }
+}); */
 button.addEventListener("click", checkValid);
+
 
 
