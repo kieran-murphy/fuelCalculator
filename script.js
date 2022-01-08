b605a0178089b309a0d40bc06b98b33ff82f364f
kmurp62rulz <kmurp62rulz@gmail.com>
2022-01-08 20:50:04 +1000

    Added Limit of 680

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 const numberGenerator = document.getElementById("random-number");
 const button = document.getElementById("btn");
 const error = document.querySelector(".error");
-const minInput = document.getElementById("min");
+const milesInput = document.getElementById("miles");
 const extraInput = document.getElementById("extra");
-
-var min = 0;
 
 
 
 function checkValid(){
 
-    miles = minInput.value;
+    miles = milesInput.value;
     extra_fuel = extraInput.value;
     if (extra_fuel === 'extra_false') {
         fuel = Math.ceil(miles * 0.48823529)
-        numberGenerator.innerHTML = fuel;
+        if (miles > 680) {
+            alert("Nautical miles exceeded 680!")
+        } else {
+            numberGenerator.innerHTML = fuel;
+        }
+        
     } else if (extra_fuel === 'extra_true') {
         fuel = Math.ceil(miles * 0.48823529) + 3
-        numberGenerator.innerHTML = fuel;
+        if (miles > 680) {
+            alert("Nautical miles exceeded 680!")
+        } else {
+            numberGenerator.innerHTML = fuel;
+        }
     }
     
 
